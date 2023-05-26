d211cfa83e8321c7d918a0c8addc712facfc0419
yanglei <715860993@qq.com>
2023-05-26 22:37:28 +0800

    样式初始化

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.default')
 
 @section('content')
-  <h1>主页面</h1>
+  <div class="bg-info p-3 p-sm-5 rounded">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+        你现在所看到的是 <a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 的示例项目主页。
+    </p>
+    <p>
+        一切，将从这里开始。
+    </p>
+    <p>
+        <a class="btn btn-lg btn-success" href="/" role="button">现在注册</a>
+    </p>
+  </div>
+
+
 @stop
 

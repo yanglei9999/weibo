3f278e8b27c4d1973529eefef6b3dd8a3b3f6572
yanglei <715860993@qq.com>
2023-05-30 22:28:56 +0800

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -2,36 +2,41 @@
 @section('title', '登录')
 
 @section('content')
-    <div class="offset-md-2 col-md-8">
-      <div class="card">
-        <div class="card-header">
-          <h5>登录</h5>
-        </div>
-        <div class="card-body">
-          @include('shared._errors')
+  <div class="offset-md-2 col-md-8">
+    <div class="card ">
+      <div class="card-header">
+        <h5>登录</h5>
+      </div>
+      <div class="card-body">
+        @include('shared._errors')
 
-          <form action="{{ route('login') }}" method="post">
-            {{ csrf_field() }}
+        <form method="POST" action="{{ route('login') }}">
+          {{ csrf_field() }}
 
-            <div class="mb-3">
-              <label for="email">邮箱：</label>
-              <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          <div class="form-group">
+            <label for="email">邮箱：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">密码：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">记住我</label>
             </div>
+          </div>
 
-            <div class="mb-3">
-              <label for="password">密码：</label>
-              <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-            </div>
+          <button type="submit" class="btn btn-primary">登录</button>
+        </form>
 
-            <button type="submit" class="btn btn-primary">登录</button>
-          </form>
+        <hr>
 
-          <hr>
-
-          <p>
-            还没账号？<a href="{{ route('signup') }}">现在注册！</a>
-          </p>
-        </div>
+        <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
       </div>
     </div>
-@endsection
+  </div>
+@stop

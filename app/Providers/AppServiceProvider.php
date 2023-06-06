576760a8072c52d35e31c6f7ef9b186bf2256ff0
yanglei <715860993@qq.com>
2023-06-06 21:48:01 +0800

    用户分页

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //分页使用bootstrap
+        \Illuminate\Pagination\Paginator::useBootstrap();
+
     }
 }
